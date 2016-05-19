5150ffa49c13d5a9c2853debc5b3a573b4253be7
Tim Moran <tim@t-moran.com>
2016-05-19 07:51:49 +0100

    Listen to UDP stream from GQRX

--- decode.py
+++ decode.py
@@ -1,59 +1,64 @@
-#import matplotlib.pyplot as plt
 import struct
+import socket
 
-max_amplitude = 0
-min_amplitude = 0
 
-index = 0
-indices = []
-raw_values = []
+def signals():
+    max_amplitude = 0
+    min_amplitude = 0
 
-min_signal = 5000
-high = False
-high_at = 0
-first_high = 0
+    index = 0
+    raw_values = []
 
-signals = []
+    min_signal = 5000
+    high = False
+    high_at = 0
+    first_high = 0
 
-with open('listening.dat', 'r') as input_stream:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("127.0.0.1", 7355))
+
+    b = ''
+
     while True:
         # 48khz 16 bit signed, le (S16LE)
-        b = input_stream.read(2)
+        data, addr = sock.recvfrom(1024)
 
-        if len(b) < 2:
-            break
+        b = b+data
 
-        value = struct.unpack("<h", b)[0]
+        while len(b) > 2:
+            value = struct.unpack("<h", b[0:2])[0]
+            b = b[2:]
 
-        #print repr(b), "=>", value
-        max_amplitude = max(max_amplitude, value)
-        min_amplitude = min(min_amplitude, value)
-        raw_values.append(value)
-        if len(raw_values) > 100:
-            raw_values = raw_values[-100:]
-        recent_min = min(raw_values[-min(len(raw_values), 100):])
-        value = value-recent_min
-        if not high and value > min_signal:
-            high = True
-            if first_high == 0:
-                first_high = index
-            high_at = index - first_high
-            #print "High at", high_at
-        elif high and value < min_signal:
-            high = False
-            #print "Low at", index-first_high, "(", (index-first_high-high_at), ")"
-            signals.append((high_at, index-first_high))
-        indices.append(index)
+            #print repr(b), "=>", value
+            max_amplitude = max(max_amplitude, value)
+            min_amplitude = min(min_amplitude, value)
+            raw_values.append(value)
+            if len(raw_values) > 100:
+                raw_values = raw_values[-100:]
+            recent_min = min(raw_values[-min(len(raw_values), 100):])
+            value = value-recent_min
+            if not high and value > min_signal:
+                high = True
+                if first_high == 0:
+                    first_high = index
+                high_at = index - first_high
+                #print "High at", high_at
+            elif high and value < min_signal:
+                high = False
+                #print "Low at", index-first_high, "(", (index-first_high-high_at), ")"
+                yield (high_at, index-first_high)
 
-        index += 1
+            index += 1
 
 sync_pulses = []
 previous_end = -1
 bits = [1]
-for signal in signals:
+for signal in signals():
     start, end = (signal)
     pulse_length = end-start
     if (previous_end != -1 and previous_end < start-50):
+        if len(bits) == 63:
+            bits.append(0)
         print "0x%x" % int(''.join(map(str, bits)), 2), bits, len(bits)
         sync_pulses = []
         bits = [1]
